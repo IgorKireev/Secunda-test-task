d75cf7bbbf87df7f137f55cd85d909d3cde1ac5a
garik <igorkireev02@mail.ru>
2025-10-06 17:20:10 +0300

    fix models -> model

--- app/infrastructure/db/__init__.py
+++ app/infrastructure/db/__init__.py
@@ -1,5 +1,5 @@
 from app.infrastructure.db.accessor import get_session
-from app.infrastructure.db.base_models import Base
+from app.infrastructure.db.base_model import Base
 
 
 __all__ = ["get_session", Base]
