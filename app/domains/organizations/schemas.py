71c8091720e0b08a041920407b55aebd66ee3a8c
garik <igorkireev02@mail.ru>
2025-10-07 16:17:20 +0300

    add create&read models

--- app/domains/organizations/schemas.py
+++ app/domains/organizations/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator, Field, ConfigDict
 import phonenumbers
 from app.domains.activities import ActivityBase
 from app.domains.buildings import BuildingBase
@@ -16,23 +16,34 @@ class PhoneNumber(BaseModel):
         return phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.E164)
 
 
-class Organizations(BaseModel):
+class OrganizationBase(BaseModel):
     title: str = Field(max_length=30)
     phone_numbers: list[PhoneNumber] = Field(default_factory=list)
-    building: BuildingBase
-    activities: list[ActivityBase] = Field(default_factory=list)
-
 
     @classmethod
     @field_validator('phone_numbers', mode='before')
     def ensure_phone_objects(cls, v: str | PhoneNumber | list[str | PhoneNumber]):
-        if isinstance(v, PhoneNumber):
-            return [v]
-        elif isinstance(v, str):
-            return [PhoneNumber(phone_number=v)]
-        elif isinstance(v, list):
-            return [
-                PhoneNumber(phone_number=p) if isinstance(p, str) else p
-                for p in v
-            ]
-        return v
+        if not v:
+            return []
+        if isinstance(v, (str, PhoneNumber)):
+            v = [v]
+        return [
+            PhoneNumber(phone_number=p) if isinstance(p, str) else p
+            for p in v
+        ]
+
+
+class OrganizationCreate(OrganizationBase):
+    building_id: int = Field(...)
+    activity_ids: list[int] = Field(default_factory=list)
+
+
+class OrganizationRead(OrganizationBase):
+    id: int
+    building: BuildingBase
+    activities: list[ActivityBase]
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        frozen=True,
+    )
