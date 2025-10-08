388dd574aa794fc38e8481163bff17f01c735a51
garik <igorkireev02@mail.ru>
2025-10-08 12:19:25 +0300

    upd imports

--- app/infrastructure/db/__init__.py
+++ app/infrastructure/db/__init__.py
@@ -1,5 +1,6 @@
 from app.infrastructure.db.accessor import get_session
+from app.infrastructure.db.association_tables import association_table
 from app.infrastructure.db.base_model import Base
 
 
-__all__ = ["get_session", Base]
+__all__ = ["get_session", "association_table", "Base" ]
