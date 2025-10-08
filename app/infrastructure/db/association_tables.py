a744cab75cd1435d9086497ef84825a4172bfb33
garik <igorkireev02@mail.ru>
2025-10-08 14:21:59 +0300

    upd fk

--- app/infrastructure/db/association_tables.py
+++ app/infrastructure/db/association_tables.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Table, ForeignKey
-from app.infrastructure.db import Base
+from app.infrastructure.db.base_model import Base
 
 
 association_table = Table(
     "association_table",
     Base.metadata,
-    Column("organization_id", ForeignKey("organization.id")),
-    Column("activity_id", ForeignKey("activities.id")),
+    Column("organization_id", ForeignKey("organizations.id"), primary_key=True),
+    Column("activity_id", ForeignKey("activities.id"), primary_key=True),
 )
