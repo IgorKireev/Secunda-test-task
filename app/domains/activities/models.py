4ea80f9a03d9fb56d7cc121fecf3cdf2b0af5049
garik <igorkireev02@mail.ru>
2025-10-08 14:20:17 +0300

    add type cheking and secondary in relation

--- app/domains/activities/models.py
+++ app/domains/activities/models.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.infrastructure import association_table, Base
+if TYPE_CHECKING:
+    from app.domains.organizations.models import Organization
 
 
 class Activity(Base):
     __tablename__ = "activities"
 
     name: Mapped[str] = mapped_column(String(30))
-    organizations: Mapped[list["association_table"]] = relationship(back_populates="activities")
+    organizations: Mapped[list["Organization"]] = relationship(
+        secondary=association_table,
+        back_populates="activities"
+    )
--- app/domains/organizations/models.py
+++ app/domains/organizations/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.infrastructure import association_table, Base
 if TYPE_CHECKING:
     from app.domains.buildings.models import Building
+    from app.domains.activities.models import Activity
 
 
 class Organization(Base):
@@ -16,12 +17,15 @@ class Organization(Base):
         cascade="all, delete-orphan",
     )
     building: Mapped["Building"] = relationship(back_populates="organization")
-    activities: Mapped[list["association_table"]] = relationship(back_populates="organizations")
+    activities: Mapped[list["Activity"]] = relationship(
+        secondary=association_table,
+        back_populates="organizations"
+    )
 
 
 class PhoneNumber(Base):
     __tablename__ = "phone_numbers"
 
-    phone_number: Mapped[str]
-    organization_id: Mapped[int] = mapped_column(ForeignKey("organization.id", ondelete="SET NULL"))
+    phone_number: Mapped[str] = mapped_column(String(12))
+    organization_id: Mapped[int] = mapped_column(ForeignKey("organizations.id", ondelete="CASCADE"))
     organization: Mapped["Organization"] = relationship(back_populates="phone_numbers")
