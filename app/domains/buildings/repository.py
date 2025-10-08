89703744aa15a177c46c2e152ca0cc6826f01916
garik <igorkireev02@mail.ru>
2025-10-08 21:14:49 +0300

    add get methods

--- app/domains/buildings/repository.py
+++ app/domains/buildings/repository.py
@@ -1,13 +1,34 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.domains import Building
 from app.domains.buildings.schemas import BuildingCreate, Coordinates
 
 
 class BuildingRepository():
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
+
+
+    async def get_buildings(self) -> list[Building]:
+        query = (
+            select(Building)
+            .options(selectinload(Building.organization))
+        )
+        buildings = await self.session.execute(query)
+        return buildings.scalars().all()
+
+
+    async def get_building(self, building_id) -> Building | None:
+        query = (
+            select(Building)
+            .filter(Building.id == building_id)
+            .options(selectinload(Building.organization))
+        )
+        building = await self.session.execute(query)
+        return building.scalar_one_or_none()
+
 
     async def create_building(self, building_data: BuildingCreate) -> Building:
         building = Building(
