895eaf42f572ed8343250cce0df3c6022199fd82
garik <igorkireev02@mail.ru>
2025-10-08 12:25:57 +0300

    add activity model

---
+++ app/domains/activities/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.infrastructure import association_table, Base
+
+
+class Activity(Base):
+    name: Mapped[str] = mapped_column(String(30))
+    organizations: Mapped[list["association_table"]] = relationship(back_populates="activities")
