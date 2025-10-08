825aa1ef9342fb3853345eb86d692631a10eb289
garik <igorkireev02@mail.ru>
2025-10-08 12:25:40 +0300

    add building model

---
+++ app/domains/buildings/models.py
@@ -0,0 +1,13 @@
+from typing import TYPE_CHECKING
+from sqlalchemy import String, DECIMAL
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.infrastructure import Base
+if TYPE_CHECKING:
+    from app.domains.organizations.models import Organization
+
+
+class Building(Base):
+    address: Mapped[str] = mapped_column(String(50))
+    latitude: Mapped[float] = mapped_column(DECIMAL(9, 6), nullable=False)
+    longitude: Mapped[float] = mapped_column(DECIMAL(9, 6), nullable=False)
+    organization: Mapped[list["Organization"]] = relationship(back_populates="building")
