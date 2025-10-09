f94f2a05a1e4abf50413ed410144a4fa6e60770b
garik <igorkireev02@mail.ru>
2025-10-09 11:17:12 +0300

    add integrity exc

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -5,3 +5,6 @@ class NotFoundError(Exception):
         super().__init__(self.detail)
 
 
+class DataIntegrityError(Exception):
+    def __init__(self, message: str = "Data integrity violation") -> None:
+        super().__init__(message)
