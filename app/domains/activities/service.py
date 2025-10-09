ef0e2e27d22284b27b4f14890244533880ec8643
garik <igorkireev02@mail.ru>
2025-10-09 11:34:18 +0300

    add get activity

--- app/domains/activities/service.py
+++ app/domains/activities/service.py
@@ -1,5 +1,6 @@
 from app.domains.activities.repository import ActivityRepository
 from app.domains.activities.schemas import ActivityRead, ActivityCreate
+from app.exceptions.exceptions import NotFoundError
 
 
 class ActivityService:
@@ -8,8 +9,15 @@ class ActivityService:
 
 
     async def get_activities(self) -> list[ActivityRead]:
-        activities_orm = await self.activity_repository.get_activities()
+        activities = await self.activity_repository.get_activities()
         return [
             ActivityRead.model_validate(activity)
-            for activity in activities_orm
+            for activity in activities
         ]
+
+
+    async def get_activity(self, activity_id: int) -> ActivityRead:
+        activity = await self.activity_repository.get_activity(activity_id)
+        if not activity:
+            raise NotFoundError(entity="Activity")
+        return ActivityRead.model_validate(activity)
