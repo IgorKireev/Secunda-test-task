f38024aacc8235e8bb8eb07c8b39b6b982f8a50f
garik <igorkireev02@mail.ru>
2025-10-09 14:38:22 +0300

    add rel dto

--- app/domains/activities/schemas.py
+++ app/domains/activities/schemas.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field, ConfigDict
 
+from app.domains.organizations.models import Organization
+from app.domains.organizations.schemas import OrganizationDTO
+
 
 class ActivityBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=30)
@@ -9,10 +12,13 @@ class ActivityCreate(ActivityBase):
     pass
 
 
-class ActivityRead(ActivityBase):
+class ActivityDTO(ActivityBase):
     id: int
 
     model_config = ConfigDict(
         from_attributes=True,
         frozen=True,
     )
+
+class ActivityRelDTO(ActivityDTO):
+    organizations: list["OrganizationDTO"]
