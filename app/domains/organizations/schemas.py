d93e97a467294cbb435b5c65ce93004747b9d46e
garik <igorkireev02@mail.ru>
2025-10-09 14:42:10 +0300

    add rel dto

--- app/domains/organizations/schemas.py
+++ app/domains/organizations/schemas.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel, field_validator, Field, ConfigDict
 import phonenumbers
 from app.domains.activities import ActivityBase
+from app.domains.activities.schemas import ActivityDTO
 from app.domains.buildings import BuildingBase
+from app.domains.buildings.schemas import BuildingDTO
+from app.domains.organizations.models import Organization
 
 
 class PhoneNumber(BaseModel):
@@ -38,12 +41,15 @@ class OrganizationCreate(OrganizationBase):
     activity_ids: list[int] = Field(default_factory=list)
 
 
-class OrganizationRead(OrganizationBase):
+class OrganizationDTO(OrganizationBase):
     id: int
-    building: BuildingBase
-    activities: list[ActivityBase]
 
     model_config = ConfigDict(
         from_attributes=True,
         frozen=True,
     )
+
+class OrganizationRelDTO(OrganizationDTO):
+    building: BuildingDTO
+    activities: list[ActivityDTO]
+
