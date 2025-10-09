d9a69f52114660202e524b4d5b093c52241d7775
garik <igorkireev02@mail.ru>
2025-10-09 11:39:25 +0300

    add create method

--- app/domains/activities/service.py
+++ app/domains/activities/service.py
@@ -1,6 +1,9 @@
+from sqlalchemy.exc import IntegrityError
+
+from app.domains import Activity
 from app.domains.activities.repository import ActivityRepository
 from app.domains.activities.schemas import ActivityRead, ActivityCreate
-from app.exceptions.exceptions import NotFoundError
+from app.exceptions.exceptions import NotFoundError, DataIntegrityError
 
 
 class ActivityService:
@@ -21,3 +24,16 @@ class ActivityService:
         if not activity:
             raise NotFoundError(entity="Activity")
         return ActivityRead.model_validate(activity)
+
+
+    async def create_activity(self, activity_data: ActivityCreate) -> ActivityRead:
+        activity_orm = Activity(
+            name=activity_data.name,
+        )
+        try:
+           activity = await self.activity_repository.create_activity(activity_orm)
+           await self.activity_repository.session.commit()
+           return ActivityRead.model_validate(activity)
+        except IntegrityError as e:
+            await self.activity_repository.session.rollback()
+            raise DataIntegrityError(f"Could not create activity: {str(e.orig)}")
