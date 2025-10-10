2d22bfff91929b653d3444dcf9dd929823dac306
garik <igorkireev02@mail.ru>
2025-10-10 13:06:08 +0300

    add exc&routers

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.exceptions.exceptions import NotFoundError, DataIntegrityError
+from app.api import (
+    api_v1_router,
+    activity_router,
+    building_router,
+    organization_router,
+)
+
+
+app = FastAPI()
+app.include_router(router=api_v1_router)
+app.include_router(router=activity_router)
+app.include_router(router=building_router)
+app.include_router(router=organization_router)
+
+
+@app.exception_handler(NotFoundError)
+async def not_found_exception_handler(request: Request, exc: NotFoundError):
+    return JSONResponse(
+        status_code=404,
+        content={"message": exc.detail}
+    )
+
+
+@app.exception_handler(DataIntegrityError)
+async def data_integrity_exception_handler(request: Request, exc: DataIntegrityError):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)}
+    )
+
+@app.get("/")
+async def main():
+    return {
+        "message": "Hello Secunda!"
+    }
