1e58bdce1f6d1beadece62979f362c4169c858d2
garik <igorkireev02@mail.ru>
2025-10-10 09:23:40 +0300

    add activities endpoints

---
+++ app/api/activities.py
@@ -0,0 +1,50 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from app.dependencies import get_activity_service
+from app.domains.activities.schemas import ActivityCreate
+from app.domains.activities.service import ActivityService
+
+
+router = APIRouter(prefix="/activities", tags=["Activities"])
+
+
+@router.get("/")
+async def get_activity(
+        activity_service: Annotated[
+            ActivityService,
+            Depends(get_activity_service)
+        ]
+    ):
+    return await activity_service.get_activities()
+
+@router.get("/{activity_id}")
+async def get_activity(
+        activity_service: Annotated[
+            ActivityService,
+            Depends(get_activity_service)
+        ],
+        activity_id: int
+    ):
+    return await activity_service.get_activity(activity_id)
+
+
+@router.post("/")
+async def create_activity(
+        activity_service: Annotated[
+            ActivityService,
+            Depends(get_activity_service)
+        ],
+        activity_data: ActivityCreate
+    ):
+    return await activity_service.create_activity(activity_data)
+
+
+@router.delete("/")
+async def delete_activity(
+        activity_service: Annotated[
+            ActivityService,
+            Depends(get_activity_service)
+        ],
+        activity_id: int
+    ):
+    return await activity_service.delete_activity(activity_id)
