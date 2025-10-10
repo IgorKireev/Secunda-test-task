e92b2948663ee2153683e2b0fba71bebac5ea6b4
garik <igorkireev02@mail.ru>
2025-10-10 10:39:47 +0300

    fix schemas

--- app/domains/activities/schemas.py
+++ app/domains/activities/schemas.py
@@ -1,8 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
 
-from app.domains.organizations.models import Organization
-from app.domains.organizations.schemas import OrganizationDTO
-
 
 class ActivityBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=30)
@@ -19,6 +16,3 @@ class ActivityDTO(ActivityBase):
         from_attributes=True,
         frozen=True,
     )
-
-class ActivityRelDTO(ActivityDTO):
-    organizations: list["OrganizationDTO"]
