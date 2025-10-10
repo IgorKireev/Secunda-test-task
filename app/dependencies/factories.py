a4b2bd58bfedaa1c284bc34001443d2e89b3d03f
garik <igorkireev02@mail.ru>
2025-10-10 13:00:56 +0300

    add dec for repository

---
+++ app/dependencies/factories.py
@@ -0,0 +1,13 @@
+from typing import Annotated, Type, TypeVar
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.infrastructure import get_session
+
+
+T = TypeVar("T")
+
+
+def repository_factory(repo_class: Type[T]):
+    async def _get_repository(session: Annotated[AsyncSession, Depends(get_session)]) -> T:
+        return repo_class(session=session)
+    return _get_repository
