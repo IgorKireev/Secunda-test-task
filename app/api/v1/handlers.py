3323308e576d7d65df341c24cae08dabdb3a36a2
garik <igorkireev02@mail.ru>
2025-10-10 11:56:36 +0300

    add tz handlers

---
+++ app/api/v1/handlers.py
@@ -0,0 +1,47 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from app.dependencies import (
+    get_organization_service,
+    get_building_service,
+    get_activity_service,
+)
+from app.domains import (
+    ActivityService,
+    BuildingService,
+    OrganizationService,
+)
+
+
+router = APIRouter(prefix="/api/v1", tags=["API"])
+
+
+@router.get("/buildings/building_id/organizations")
+async def get_organizations_by_building(
+        building_service: Annotated[
+            BuildingService,
+            Depends(get_building_service)
+        ],
+        building_id: int
+    ):
+    return await building_service.get_building(building_id, organizations=True)
+
+@router.get("/activities/{activity_id}/organizations")
+async def get_organizations_by_activity(
+        activity_service: Annotated[
+            ActivityService,
+            Depends(get_activity_service)
+        ],
+        activity_id: int
+    ):
+    return await activity_service.get_activity(activity_id, organizations=True)
+
+@router.get("/organizations/search/title")
+async def get_organization_by_title(
+        organization_service: Annotated[
+            OrganizationService,
+            Depends(get_organization_service)
+        ],
+        title: str
+    ):
+    return await organization_service.get_organization_by_title(title)
+
