dc6652305f02e0f55584d3ce6d75c19c3adf1b45
garik <igorkireev02@mail.ru>
2025-10-10 13:01:46 +0300

    add factory services

---
+++ app/dependencies/services.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+from fastapi import Depends
+from app.dependencies.repositories import (
+    get_activity_repository,
+    get_building_repository,
+    get_organization_repository,
+)
+from app.domains import (
+    ActivityRepository,
+    ActivityService,
+    BuildingRepository,
+    BuildingService,
+    OrganizationRepository,
+    OrganizationService,
+)
+
+
+def get_activity_service(
+        activity_repository: Annotated[
+            ActivityRepository,
+            Depends(get_activity_repository)
+        ]
+    ):
+    return ActivityService(
+        activity_repository=activity_repository,
+    )
+
+async def get_building_service(
+        building_repository: Annotated[
+            BuildingRepository,
+            Depends(get_building_repository)
+        ]
+    ):
+    return BuildingService(
+        building_repository=building_repository
+    )
+
+def get_organization_service(
+        organization_repository: Annotated[
+            OrganizationRepository,
+            Depends(get_organization_repository)
+        ],
+        activity_service: Annotated[ActivityService, Depends(get_activity_service)],
+    ):
+    return OrganizationService(
+        organization_repository=organization_repository,
+        activity_service=activity_service
+    )
