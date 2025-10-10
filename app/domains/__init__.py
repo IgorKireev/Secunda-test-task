9c20f83b707886cebb322053fbbce40f6c8bd942
garik <igorkireev02@mail.ru>
2025-10-10 12:06:57 +0300

    add imports

---
+++ app/domains/__init__.py
@@ -0,0 +1,35 @@
+from app.domains.activities import (
+    Activity,
+    ActivityRepository,
+    ActivityDTO,
+    ActivityService,
+)
+from app.domains.buildings import (
+    Building,
+    BuildingRepository,
+    BuildingDTO,
+    BuildingService,
+)
+from app.domains.organizations import (
+    Organization,
+    OrganizationRepository,
+    OrganizationDTO,
+    OrganizationService,
+)
+
+
+__all__ = [
+    "Activity",
+    "ActivityRepository",
+    "ActivityDTO",
+    "ActivityService",
+    "Building",
+    "BuildingRepository",
+    "BuildingDTO",
+    "BuildingRepository",
+    "BuildingService",
+    "Organization",
+    "OrganizationRepository",
+    "OrganizationDTO",
+    "OrganizationService"
+]
