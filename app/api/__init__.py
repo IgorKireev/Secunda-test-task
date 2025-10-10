61c9866806c6d64e8e5a1631bf29b93c0123148d
garik <igorkireev02@mail.ru>
2025-10-10 11:51:50 +0300

    add imports handlers

---
+++ app/api/__init__.py
@@ -0,0 +1,13 @@
+from app.api.v1.handlers import router as api_v1_router
+from app.api.activities import router as activity_router
+from app.api.buildings import router as building_router
+from app.api.organizations import router as organization_router
+
+
+
+__all__ = [
+    "api_v1_router",
+    "activity_router",
+    "building_router",
+    "organization_router"
+]
