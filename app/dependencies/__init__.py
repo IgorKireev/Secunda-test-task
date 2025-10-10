a49d2f945b82ebb9fede94675653aa11667f23fd
garik <igorkireev02@mail.ru>
2025-10-10 13:00:27 +0300

    add imports

---
+++ app/dependencies/__init__.py
@@ -0,0 +1,12 @@
+from app.dependencies.services import (
+    get_building_service,
+    get_activity_service,
+    get_organization_service
+)
+
+
+__all__ = [
+    "get_activity_service",
+    "get_building_service",
+    "get_organization_service"
+]
