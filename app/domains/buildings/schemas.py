82ec62ccb9b395a942c6f741f000a43c740ce220
garik <igorkireev02@mail.ru>
2025-10-10 10:38:57 +0300

    address max length 30->50

--- app/domains/buildings/schemas.py
+++ app/domains/buildings/schemas.py
@@ -1,10 +1,8 @@
 from pydantic import BaseModel, condecimal, Field, ConfigDict
 
-from app.domains.organizations.schemas import OrganizationDTO
-
 
 class BuildingBase(BaseModel):
-    address: str = Field(min_length=5, max_length=30)
+    address: str = Field(min_length=5, max_length=50)
     latitude: condecimal(ge=-90, le=90)
     longitude: condecimal(ge=-180, le=180)
 
@@ -20,6 +18,3 @@ class BuildingDTO(BuildingBase):
         from_attributes=True,
         frozen=True,
     )
-
-class BuildingRelDTO(BuildingDTO):
-    organizations: list["OrganizationDTO"]
