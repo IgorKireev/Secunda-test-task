e1ebf1b5677ecacb665cbfebd2d8cc724cd4f19d
garik <igorkireev02@mail.ru>
2025-10-18 16:24:57 +0300

    formatting black

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,5 @@
 from app.api.v1.handlers import router as api_v1_router
+from app.api.auth import router as auth_router
 from app.api.activities import router as activity_router
 from app.api.buildings import router as building_router
 from app.api.organizations import router as organization_router
@@ -7,8 +8,9 @@ from app.api.users import router as user_router
 
 __all__ = [
     "api_v1_router",
+    "auth_router",
     "activity_router",
     "building_router",
     "organization_router",
-    "user_router"
+    "user_router",
 ]
