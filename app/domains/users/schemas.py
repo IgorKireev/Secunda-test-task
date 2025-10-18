15a9a03b69983580fdb42980cc0881a30b270583
garik <igorkireev02@mail.ru>
2025-10-18 16:05:41 +0300

    add role schema

--- app/domains/users/schemas.py
+++ app/domains/users/schemas.py
@@ -1,35 +1,50 @@
-from pydantic import BaseModel, Field, EmailStr, ConfigDict, field_validator
+from pydantic import BaseModel, Field, EmailStr, ConfigDict, model_validator
+from app.domains.users.models import Role
 
 
 class UserBase(BaseModel):
     name: str = Field(min_length=1, max_length=30)
 
+
 class EmailSchemaMixin(BaseModel):
     email: EmailStr
+
+
+class RoleSchemaMixin(BaseModel):
+    role: Role
+
 
 class PasswordSchemaMixin(BaseModel):
     password: str
 
-    @classmethod
-    @field_validator("password")
-    def validate_password(cls, value: str) -> str:
-        if len(value) < 8:
+    @model_validator(mode="after")
+    def validate_password(self):
+        if len(self.password) < 8:
             raise ValueError("Password must be at least 8 characters")
-        return value
+        return self
 
-class UserCreate(UserBase, EmailSchemaMixin, PasswordSchemaMixin):
+
+class UserRegister(UserBase, EmailSchemaMixin, PasswordSchemaMixin):
     pass
+
+
+class UserCreate(UserRegister, RoleSchemaMixin):
+    pass
+
 
 class UserLogin(EmailSchemaMixin, PasswordSchemaMixin):
     pass
 
-class UserDTO(UserBase, EmailSchemaMixin):
+
+class UserDTO(UserBase, EmailSchemaMixin, RoleSchemaMixin):
     id: int
+
 
     model_config = ConfigDict(
         from_attributes=True,
         frozen=True,
     )
+
 
 class UserResponse(UserDTO):
     pass
